fe48268d8ac28c38ec689a16f5686be349649f09
whoodongpyo <vvhooping@gmail.com>
2023-06-02 11:04:46 +0900

    :construction: 루트 Module 에서 TypeORM 설정을 import

--- src/configs/typeORMConfig.ts
+++ src/configs/typeORMConfig.ts
@@ -1,6 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeormConfig: TypeOrmModuleOptions = {
+export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
