bc77b9237c450fab75007f2b6eccd0ef7fc59c36
whoodongpyo <vvhooping@gmail.com>
2023-06-10 17:32:43 +0900

    :safety_vest: 여행 일정 작성 시 유효성 검사 추가

--- src/schedules/dto/create-schedule.dto.ts
+++ src/schedules/dto/create-schedule.dto.ts
@@ -1,28 +1,39 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { ScheduleStatus } from '../../types/ScheduleStatus.enum';
 
 export class CreateScheduleDto {
   @ApiProperty({ description: '일정 제목', example: '영희의 우도 여행 #1' })
-  @IsString()
+  @IsString({ message: '일정 제목은 문자열이어야 합니다.' })
+  @IsNotEmpty({ message: '일정 제목은 비어있지 않아야 합니다.' })
   title: string;
 
   @ApiProperty({
     description: '한줄 소개',
     example: '1박2일로 떠나는 우도 여행',
   })
-  @IsString()
+  @IsNotEmpty({ message: '일정 한줄요약은 비어있지 않아야 합니다.' })
+  @IsString({ message: '일정 한줄요약은 문자열이어야 합니다.' })
   summary: string;
 
-  @ApiProperty({ description: '여행 기간 (단위: 일)', example: 3 })
-  @IsNumber()
+  @ApiProperty({ description: '일정 기간 (단위: 일)', example: 3 })
+  @IsNotEmpty({ message: '일정 기간은 비어있지 않아야 합니다.' })
+  @IsNumber({}, { message: '일정 기간은 숫자이어야 합니다.' })
+  @Min(1, { message: '일정 기간은 0보다 커야 합니다.' })
   duration: number;
 
-  @ApiProperty({ description: '여행 시작일', example: '2023-01-01' })
-  @IsString()
+  @ApiProperty({
+    description: '일정 시작일',
+    example: 'Fri Jun 09 2023 20:38:15 GMT+0900 (한국 표준시)',
+  })
+  @IsNotEmpty({ message: '일정 시작일은 비어있지 않아야 합니다.' })
+  @IsString({ message: '일정 시작일은 문자열이어야 합니다.' })
   start_date: string;
 
-  @ApiProperty({ description: '여행 종료일', example: '2023-01-02' })
-  @IsString()
+  @ApiProperty({
+    description: '일정 종료일',
+    example: 'Fri Jun 16 2023 20:38:15 GMT+0900 (한국 표준시)',
+  })
+  @IsNotEmpty({ message: '일정 종료일은 비어있지 않아야 합니다.' })
+  @IsString({ message: '일정 종료일은 문자열이어야 합니다.' })
   end_date: string;
 }
